c42aff131e71e05aebc19aabcb6b19a8cb223f5e
DevOps\Developer <bobbanovski@gmail.com>
2017-04-07 23:20:09 +1000

    refactoring of attack function

--- monsterSlayer/app.js
+++ monsterSlayer/app.js
@@ -10,6 +10,45 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
-        }
+        },
+        attack: function() {
+            this.monsterHealth -= Math.trunc(this.calculateDamage(3,10));
+            if(this.checkWin()) {
+                return;
+            };
+            
+            this.playerHealth -= Math.trunc(this.calculateDamage(2,14));
+            this.checkWin();            
+        },
+        special: function() {
+
+        },
+        heal: function() {
+
+        },
+        surrender: function() {
+
+        },
+        calculateDamage: function(min, max) {
+            return (Math.random() * (max - min)) + min;
+        },
+        checkWin: function() {
+            if (this.monsterHealth <= 0) {
+                if (confirm('You won! New Game?')){
+                    this.beginGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if (this.playerHealth <= 0) {
+                if (confirm('You lost! New Game?')){
+                    this.beginGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
+        },
     }
 })
